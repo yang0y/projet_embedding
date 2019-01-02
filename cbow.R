b9c5dd7aa9792e2ad86b334842059ae737eb1dd5
yang yang <44868838+yang0y@users.noreply.github.com>
2019-01-02 07:27:53 +0100

    Add files via upload

---
+++ cbow.R
@@ -0,0 +1,127 @@
+library(text2vec)
+library(Matrix)
+library(sparsesvd)
+library(NLP)
+library(tm)
+library(MASS)
+
+
+# Generation du jeu de donnees d’apprentissage
+max_vocabulary_size <- 30000
+corpus <- readLines('text8', n=1, warn=FALSE)
+# Tokenisation
+corpus <- word_tokenizer(corpus)[[1]]
+# Largeur de la fenêtre contexte
+l <- 3
+# Ajout NA au début et a la fin
+corpus <- c(rep("NA", l),corpus,rep("NA", l))
+#Suppression des stopwords
+stop_words <- tm::stopwords(kind = "en")
+corpus <- corpus[!(corpus %in% stop_words)]
+
+dict <- unique(corpus)
+dict1<- unique(corpus[1:30000])
+#trouver l'indice de chaque mot correspond
+require(parallel)
+cls <- makeCluster(detectCores())
+clusterExport(cls, c("dict"), envir = environment())
+corpus_indice <- unlist(parLapply(cls, corpus[1:max_vocabulary_size], function(x){which(x == dict)}))
+stopCluster(cls)
+#Construction jeu de donnees
+D <- matrix(0, nrow = length(corpus_indice)-2*l, ncol = 1+2*l)
+colnames(D) <- c("target", paste0("context_", 1:(2*l)))
+
+for(w in 1:(max_vocabulary_size-2*l)){
+  D[w,] <- c(corpus_indice[w+l], corpus_indice[w+l-(l:1)], corpus_indice[w+l+(1:l)])
+}
+
+#function softmax
+softmax <- function(U, alpha) {
+  # scalaires Ui * alpha (un pour chaque ligne de U)
+  scal <- exp(U %*% alpha)
+  total <- sum(scal)
+  
+  res <- as.vector(scal/total)
+  return(res)
+}
+
+#Le gradient de l en Ui
+grad_u <- function(softmax, alpha) {
+  return(alpha * (1-softmax))
+}
+
+#Le gradient de l en Vj
+grad_v <- function(U, i, e, l) {
+  ui <- U[i,]
+  return((ui - e)/(2*l))
+}
+
+p<-6
+my_sgd <- function(D, vocab, p, n_iter, eta = 0.025) {
+  # Initialiser aléatoirement U et V
+  n <- length(vocab)
+  U <- matrix(runif(n*p,-0.2,0.2), nrow = n, ncol = p)
+  V <- matrix(runif(n*p,-0.2,0.2), nrow = n, ncol = p)
+  # Répéter iter fois
+  for(iter in 1:n_iter){
+    # Mélanger aléatoirement D
+    order <- sample(1:nrow(D), nrow(D), replace = F)
+    Dp <- D[order, ]
+    
+    # Pour chaque paire wi et contexte, mettre à jour les vecteurs selon les formule
+    for(row in 1:nrow(D)){
+      # ID du mot cible
+      i <- Dp[row, 1]
+      # ID des mots contexte
+      j <- Dp[row, -1]
+      
+      # Calcul du alpha contexte
+      alpha <- apply(V[j,], 2, sum)/(2*l)
+      # Calcul des Softmax
+      softmax <- softmax(U, alpha)
+      
+      # MAJ de Ui
+      U[i,] <- U[i,] + eta * grad_u(softmax[i], alpha)
+      
+      # Pour chaque mots contexte
+      for(word in 1:(ncol(D)-1)){
+        # ID du mot cible
+        jl <- Dp[row, word + 1]
+        # MAJ de Vjl
+        s_ui <- colSums(U * softmax)
+        V[jl,] <- V[jl,] + eta * grad_v(U, i, s_ui, l)
+      }
+    }
+  }
+  
+  return(list(U=U,V=V))
+}
+
+# test my_sgd
+res <- sgd_CBOW(D, dict,6,10,0.02)
+U<-res$U
+V<-res$V
+
+cosine_similarity <- function(v1, v2){
+  dot_product <- v1 %*% v2
+  norm_prod <- sqrt(sum(v1**2)) * sqrt(sum(v2**2))
+  return(as.numeric(dot_product / norm_prod))
+}
+
+find_closest_words <- function(v, n=5){
+  similarity <- numeric(nrow(U))
+  for(i in 1:nrow(U)){
+    similarity[i] <- cosine_similarity(v, U[i, ])
+  }
+  ordered_words <- dict1[order(-similarity)]
+  return(ordered_words[1:n])
+}
+
+resolve_analogy <- function(word_a, word_b, word_c, n=1){
+  word_d <- U[match(word_b, dict), ] - U[match(word_a, dict), ] + U[match(word_c, dict), ]
+  return(find_closest_words(word_d, n))
+}
+
+
+cosine_similarity(U[match('cat', dict), ], U[match('car', dict), ])
+resolve_analogy('father','mother', 'son')
