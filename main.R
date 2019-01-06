5cb7ee81202a52ad953f238bee66fafe420014fb
yang yang <44868838+yang0y@users.noreply.github.com>
2019-01-06 15:36:35 +0100

    Update main.R

--- main.R
+++ main.R
@@ -2,8 +2,8 @@ rm(list=ls())
 source("cbow.R")
 
 # on choisit l=5 comme skip-gram en seance 3
-data<-generation_data("../text8",5)
-res <- my_sgd(data$D, data$vocab, d = 100, n_iter = 5)
+data<-generation_data("text8",5 ,size_text = 100000)
+res <- my_sgd(data$D, data$vocab, d = 100, n_iter = 3)
 
 # Exportation pour Shiny
 vectors_cbow <- res$U
